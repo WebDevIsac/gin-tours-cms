d033de18fd863643e5b3138522f4a38074364ff7
Isac Larsson <isaclarsson98@gmail.com>
2021-11-24 20:34:48 +0100

    Remove old schemas and add new ones

--- schemas/schema.js
+++ schemas/schema.js
@@ -4,15 +4,13 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // We import object and document schemas
-import blockContent from './blockContent';
-import category from './category';
-import product from './product';
-import vendor from './vendor';
-import productVariant from './productVariant';
 
-import localeString from './locale/String';
-import localeText from './locale/Text';
-import localeBlockContent from './locale/BlockContent';
+import figure from './figure';
+import richText from './richText';
+
+import products from './products';
+import recipes from './recipes';
+import distilleries from './distilleries';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -21,17 +19,13 @@ export default createSchema({
     // Then proceed to concatenate our document type
     // to the ones provided by any plugins that are installed
     types: schemaTypes.concat([
-        // The following are document types which will appear
-        // in the studio.
-        product,
-        vendor,
-        category,
-        // When added to this list, object types can be used as
-        // { type: 'typename' } in other document schemas
-        blockContent,
-        localeText,
-        localeBlockContent,
-        localeString,
-        productVariant,
+        // Used objects
+        figure,
+        richText,
+
+        // Documents
+        recipes,
+        products,
+        distilleries,
     ]),
 });
