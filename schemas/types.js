801a4cb30df8aea10ed8d44ac3bcd86084901d9a
Isac Larsson <isaclarsson98@gmail.com>
2022-01-22 16:06:42 +0100

    Create new types document

---
+++ schemas/types.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'types',
+    title: 'Types',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Title',
+        },
+        {
+            name: 'text',
+            title: 'Text',
+            type: 'richText',
+        },
+    ],
+};
